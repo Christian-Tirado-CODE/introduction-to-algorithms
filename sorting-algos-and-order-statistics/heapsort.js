41b888b0e3006c0b415361cb992ecb395a8f1665
Christian Tirado <tiradoc02@gmail.com>
2024-06-04 05:33:33 -0400

    Heapsort: second attempt.

--- sorting-algos-and-order-statistics/heapsort.js
+++ sorting-algos-and-order-statistics/heapsort.js
@@ -1,3 +1,51 @@
-//1st session(2 min).
+//2nd session(20 min).
 
-// I don't remember much about this algo so I gave up :(
+function parent(i){
+    return Math.floor(i/2);
+}
+
+function left(i){
+    return 2*i;
+}
+
+function right(i){
+   return (2*i) + 1;
+}
+
+function max_heapify(A, i){
+    const l = left(i);
+    const r = right(i);
+    let largest;
+    if(l <= A.heap_size && A[l] > A[i]){
+        largest = l;
+    } else if(r <= A.heap_size && A[r] > A[i]) {
+        largest = r;
+    } else {
+        largest = i;
+    }
+
+    if(largest !== i){
+        const temp = A[i];
+        A[i] = A[largest];
+        A[largest] = temp;
+        
+        max_heapify(A, largest);
+    }
+}
+
+function build_max_heap(A, n){
+    for (let i = Math.floor(n/2); i > 0; i--) {
+        A.heap_size++;
+        max_heapify(A, i);
+    }
+}
+
+function heapsort(A, n){
+    build_max_heap(A, n);
+    for (let i = n; i > 1; i--) {
+        let temp = A[i];
+        A[i] = A[1];
+        A[1] = temp;
+        A.heap_size--;
+    }
+}
